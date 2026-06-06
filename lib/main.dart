bcbd9889d036c74a046535d0bb3abb69e683a51e
tanachok <tanachok114@gmail.com>
2026-06-06 14:24:10 +0700

    step 4 seperate home wiget to own

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:learn_flutter_68_2/screen/home.dart';
+
 void main() {
   //runApp(const MyApp());
   //const app = MaterialApp(title: "หัวข้อ", home: Text("hello world"));
@@ -17,27 +19,4 @@ void main() {
       ),
     ),
   );
-}
-
-class Home extends StatelessWidget {
-  const Home({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        color: Colors.purpleAccent,
-        margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-        padding: EdgeInsets.all(50),
-        child: Text(
-          "Hello World 3",
-          style: TextStyle(
-            fontSize: 30,
-            color: Colors.white,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-      ),
-    );
-  }
 }
