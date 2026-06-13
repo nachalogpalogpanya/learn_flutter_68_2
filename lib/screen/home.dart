80d606d7a2eab27d119c03a447e13ec9025718ff
tanachok <tanachok114@gmail.com>
2026-06-13 11:23:16 +0700

    Step 7 button wiget

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -5,25 +5,70 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    //return Row(
-    return Column(
-      //mainAxisAlignment: MainAxisAlignment.start,
-      //mainAxisAlignment: MainAxisAlignment.end,
-      //mainAxisAlignment: MainAxisAlignment.spaceAround,
-      //mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children:[
-        Image.asset("assets/images/flower.webp",width:200,height: 200),
-        SizedBox(height: 20,),
-        Image.network(
-          "https://hips.hearstapps.com/hmg-prod/images/pale-purple-cosmos-flower-1656258166.jpeg?crop=1.00xw:0.834xh;0,0.160xh",
-          width: 200,
-          height: 200,
-        )
-       
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          TextButton(
+            //style: TextButton.styleFrom(
+            style: TextButton.styleFrom(
 
-      ],
+              foregroundColor: Colors.green,
+
+            ),
+            onPressed: () {
+              print("กดป๋มแล้ว TextButton");
+            },
+            child: Text(
+              "กดปุ๋มนี้",
+              style: TextStyle(fontSize:20,fontWeight:FontWeight.bold),
+            ),
+          ),
+          const SizedBox(height: 20),
+          FilledButton(
+            //style: TextButton.styleFrom(
+            style: FilledButton.styleFrom(
+
+              foregroundColor: Colors.white,
+            ),
+            onPressed: () {
+              print("กดป๋มแล้ว FilledButton");
+            },
+            child: Text(
+              "กดปุ๋มนี้",
+              style: TextStyle(fontSize:20,fontWeight:FontWeight.bold),
+            ),
+          ),          const SizedBox(height: 20),
+          OutlinedButton(
+            //style: TextButton.styleFrom(
+            style: OutlinedButton.styleFrom(
+
+              foregroundColor: Colors.blue,
+            ),
+            onPressed: () {
+              print("กดป๋มแล้ว OutlinedButton");
+            },
+            child: Text(
+              "กดปุ๋มนี้",
+              style: TextStyle(fontSize:20,fontWeight:FontWeight.bold),
+            ),
+          ),
+          ElevatedButton(
+            //style: TextButton.styleFrom(
+            style: ElevatedButton.styleFrom(
+
+              foregroundColor: Colors.pink,
+            ),
+            onPressed: () {
+              print("กดป๋มแล้ว ElevatedButton");
+            },
+            child: Text(
+              "กดปุ๋มนี้",
+              style: TextStyle(fontSize:20,fontWeight:FontWeight.bold),
+            ),
+          ),
+        ],
+      ),
     );
   }
-} 
+}
