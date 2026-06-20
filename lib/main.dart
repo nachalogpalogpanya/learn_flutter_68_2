11fd9ff6f70c0a6098b777aa22868bfa028ca638
tanachok <tanachok114@gmail.com>
2026-06-20 11:19:19 +0700

    step 13

--- lib/main.dart
+++ lib/main.dart
@@ -2,21 +2,24 @@ import 'package:flutter/material.dart';
 
 import 'package:learn_flutter_68_2/screen/home.dart';
 import 'package:learn_flutter_68_2/screen/item.dart';
+import 'package:learn_flutter_68_2/screen/appForm.dart';
+
 void main() {
-  //runApp(const MyApp());
-  //const app = MaterialApp(title: "หัวข้อ", home: Text("hello world"));
-  //runApp(app);
-  runApp(
-    MaterialApp(
-      title: "หัวข้อ",
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("หัวข้อ"),
-          backgroundColor: Colors.pinkAccent,
-          centerTitle: true,
-        ),
-        body: Item(),
-      ),
-    ),
-  );
+  // //runApp(const MyApp());
+  // //const app = MaterialApp(title: "หัวข้อ", home: Text("hello world"));
+  // //runApp(app);
+  // runApp(
+  //   MaterialApp(
+  //     title: "หัวข้อ",
+  //     home: Scaffold(
+  //       appBar: AppBar(
+  //         title: Text("หัวข้อ"),
+  //         backgroundColor: Colors.pinkAccent,
+  //         centerTitle: true,
+  //       ),
+  //       body: Item(),
+  //     ),
+  //   ),
+  // );
+  runApp(AddForm());
 }
