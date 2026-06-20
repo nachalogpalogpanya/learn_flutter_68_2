2ff1a452f03b6bb9fee4e3bb87d234e888c58ea4
tanachok <tanachok114@gmail.com>
2026-06-20 10:25:11 +0700

    step 12

--- lib/screen/item.dart
+++ lib/screen/item.dart
@@ -1,62 +1,59 @@
-  import 'package:flutter/material.dart';
-  import 'package:learn_flutter_68_2/model/person.dart';
+import 'package:flutter/material.dart';
+import 'package:learn_flutter_68_2/model/person.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-  class Item extends StatefulWidget {
-    const Item({super.key});
+class Item extends StatefulWidget {
+  const Item({super.key});
 
-    @override
-    State<Item> createState() => _ItemState();
+  @override
+  State<Item> createState() => _ItemState();
+}
+
+class _ItemState extends State<Item> {
+  List data = ["สมหมาย", "สมศรี", "สมปอง", "สมจิตร", "สมพร"];
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: people.length,
+      itemBuilder: (content, index) {
+        return Container(
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.pink),
+            borderRadius: BorderRadius.circular(10),
+          ),
+          margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          padding: EdgeInsets.all(40),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "ชื่อ: ${people[index].name}",
+                    style: GoogleFonts.kanit(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    "อายุ: ${people[index].age}",
+                    style: GoogleFonts.kanit(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    "อาชีพ: ${people[index].job.title}",
+                    style: GoogleFonts.kanit(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+              Image.asset(
+                people[index].job.image,
+                width: 80,
+                height: 80,
+                fit: BoxFit.cover,
+              ),
+            ],
+          ),
+        );
+      },
+    );
   }
-
-  class _ItemState extends State<Item> {
-    List data = ["สมหมาย", "สมศรี", "สมปอง", "สมจิตร", "สมพร"];
-
-    @override
-    Widget build(BuildContext context) {
-      return ListView.builder(
-        itemCount: people.length,
-        itemBuilder: (content, index) {
-          return Container(
-            decoration: BoxDecoration(
-              border: Border.all(color: Colors.pink),
-              borderRadius: BorderRadius.circular(10),
-            ),
-            margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-            padding: EdgeInsets.all(40),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      "ชื่อ: ${people[index].name}",
-                      style:
-                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                    ),
-                    Text(
-                      "อายุ: ${people[index].age}",
-                      style:
-                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                    ),
-                    Text(
-                      "อาชีพ: ${people[index].job.title}",
-                      style:
-                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                    ),
-                  ],
-                ),
-                Image.asset(
-                  people[index].job.image,
-                  width:80,
-                  height:80,
-                  fit:BoxFit.cover
-                )
-              ],
-            )
-            
-          );
-        },
-      );
-    }
-  }
+}
